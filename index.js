c1d6f257e7be73acf183274c7fbfe4e47666c8f1
Your Name <your@email.com>
2020-11-12 05:25:47 +0000

    sum of array with flatten

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+function* flatten(array, depth) {
+  if (depth === undefined) {
+    depth = 1;
+  }
+  for (const item of array) {
+    if (Array.isArray(item) && depth > 0) {
+      yield* flatten(item, depth - 1);
+    } else {
+      yield item;
+    }
+  }
+}
+
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let arrayNew = [...flatten(array, Infinity)];
+  console.log(arrayNew);
+  let total = 0;
+  for (const num of arrayNew) {
+    total += num;
+  }
+  return total;
 }
 
 module.exports = sumItems;
